476cd44b220167e0c8f7ec7553f6485171acc07a
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-17 07:46:48 +0200

    feat: ✨ add markdown devenv recipe with linting and formatting

---
+++ src/markdown.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+
+  devcontainer.settings.customizations.vscode = {
+    extensions = [ "DavidAnson.vscode-markdownlint" ];
+  };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    # Markdown content
+    markdownlint.enable = true;
+    mdformat = {
+      enable = true;
+      package = pkgs.pythonPackages.mdformat;
+      extraPackages = with pkgs.pythonPackages; [
+        mdformat-frontmatter
+        mdformat-tables
+      ];
+    };
+
+    cspell = {
+      enable = true;
+      files = ".*\.md$";
+    };
+  };
+
+  files.".mdformat".toml = ''
+    # .mdformat.toml
+    #
+    # This file shows the default values and is equivalent to having
+    # no configuration file at all. Change the values for non-default
+    # behavior.
+    #
+    wrap = "keep"       # possible values: {"keep", "no", INTEGER}
+    number = true       # possible values: {false, true}
+    end_of_line = "lf"  # possible values: {"lf", "crlf", "keep"}
+
+  '';
+}
