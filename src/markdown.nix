203fa01953923a677f3b65f000b6a2df8bc222a2
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-18 08:41:28 +0200

    fix(markdown): 🙈 add tasks to automatically add .mdformat.toml file to devenv root .gitignore

--- src/markdown.nix
+++ src/markdown.nix
@@ -29,19 +29,52 @@ in
 
   # https://devenv.sh/tasks/
   tasks = {
-    "ci:lint:markdownlint" = {
-      description = "Lint *.md files with markdownlint";
-      exec = ''${config.git-hooks.hooks.markdownlint.package}/bin/markdownlint --json --output "$DEVENV_TASK_OUTPUT_FILE"'';
-    };
-    "ci:format:mdformat" = {
-      description = "Format *.md files with mdformat";
-      exec = "${config.git-hooks.hooks.mdformat.package}/bin/mdformat '${config.env.DEVENV_ROOT}'";
+    "ci:lint:markdownlint" =
+      let
+        inherit (config.git-hooks.hooks.markdownlint) package;
+      in
+      {
+        description = "Lint *.md files with markdownlint";
+        exec = ''
+          set -o 'errexit' -o 'pipefail'
+          ${package}/bin/markdownlint --json --output "$DEVENV_TASK_OUTPUT_FILE"
+        '';
+      };
+
+    "ci:format:mdformat" =
+      let
+        inherit (config.git-hooks.hooks.mdformat) package;
+      in
+      {
+        description = "Format *.md files with mdformat";
+        exec = ''
+          set -o 'errexit' -o 'pipefail'
+          ${package}/bin/mdformat '${config.env.DEVENV_ROOT}'
+        '';
+      };
+
+    "devenv-recipes:enterShell:ignore-mdformat-toml" = {
+      description = "Add .mdformat.toml to .gitignore";
+      before = [ "devenv:enterShell" ];
+      exec = ''
+        set -o 'errexit' -o 'pipefail'
+        grep --quiet '^.mdformat.toml$' '${config.env.DEVENV_ROOT}/.gitignore' ||
+          tee --append '${config.env.DEVENV_ROOT}/.gitignore' <<EOF
+
+        # Ignore .mdformat.toml, part of the "markdown.nix" Biapy devenv recipe
+        .mdformat.toml
+
+        EOF
+      '';
     };
   };
 
-  files.".mdformat.toml".toml = {
-    wrap = "keep"; # possible values: {"keep", "no", INTEGER}
-    number = true; # possible values: {false, true}
-    end_of_line = "lf"; # possible values: {"lf", "crlf", "keep"}
+  files = {
+    # https://mdformat.readthedocs.io/en/stable/users/configuration_file.html
+    ".mdformat.toml".toml = {
+      wrap = "keep"; # possible values: {"keep", "no", INTEGER}
+      number = true; # possible values: {false, true}
+      end_of_line = "lf"; # possible values: {"lf", "crlf", "keep"}
+    };
   };
 }
