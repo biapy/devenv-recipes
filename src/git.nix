8d0024e73a248cc7d1eaf6c6c8adb6264a6e854a
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-18 08:49:35 +0200

    style(git): 🎨 reorganize configuration for clarity and consistency

--- src/git.nix
+++ src/git.nix
@@ -3,23 +3,24 @@ let
   inherit (pkgs) git;
 in
 {
-  difftastic.enable = true;
-  delta.enable = true;
-
   # https://devenv.sh/packages/
-  packages = [
-    git
-  ];
+  packages = [ git ];
+
+  # https://devenv.sh/integrations/difftastic/
+  difftastic.enable = true;
+
+  # https://devenv.sh/integrations/delta/
+  delta.enable = true;
 
   # https://devenv.sh/tasks/
   tasks = {
     "devenv-recipes:enterTest:git-version" = {
       description = "Test available git command version match devenv git package";
+      before = [ "devenv:enterTest" ];
       exec = ''
         set -o 'errexit' -o 'pipefail'
         git --version | grep --color=auto "${git.version}"
       '';
-      before = [ "devenv:enterTest" ];
     };
   };
 }
