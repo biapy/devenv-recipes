1ee2f8577a76fc03cddec14090fb15dd99e7d3a0
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-24 09:25:16 +0200

    feat(utils.composer-bin): 🏗️ add composer.json and config file initialization tasks templates

--- src/utils/composer-bin.nix
+++ src/utils/composer-bin.nix
@@ -1,24 +1,59 @@
 { config, ... }:
 let
+  tasks = import ./tasks.nix { inherit config; };
   working-dir = "${config.env.DEVENV_ROOT}";
   composer-binary = "${config.languages.php.packages.composer}/bin/composer";
 in
 {
-  installTask = name: namespace: {
-    description = "Install ${name}";
-    before = [
-      "devenv:enterShell"
 
-    ];
-    after = [
-      "devenv-recipes:enterShell:initialize:composer-bin"
-      "devenv-recipes:enterShell:initialize:composer-bin:${namespace}"
-      "devenv-recipes:enterShell:install:composer"
-    ];
-    exec = ''
-      set -o 'errexit'
-      cd '${working-dir}'
-      '${composer-binary}' 'bin' '${namespace}' 'install'
-    '';
-  };
+  initializeComposerJsonTask =
+    {
+      name,
+      namespace,
+      composerJsonPath,
+    }:
+    {
+      "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:composer.json" = {
+        description = "Initialize ${name} composer.json";
+        before = [ "devenv:enterShell" ];
+        exec = tasks.initializeFile "vendor-bin/${namespace}/composer.json" composerJsonPath;
+      };
+    };
+
+  initializeConfigFileTask =
+    {
+      name,
+      namespace,
+      configFile,
+      configFilePath,
+    }:
+    {
+      "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:configuration" = {
+        description = ''Initialize ${name} "${configFile}" configuration file'';
+        before = [ "devenv:enterShell" ];
+        exec = tasks.initializeFile configFile configFilePath;
+      };
+    };
+
+  installTask =
+    { name, namespace }:
+    {
+      "devenv-recipes:enterShell:install:composer-bin:${namespace}" = {
+        description = "Install ${name}";
+        before = [
+          "devenv:enterShell"
+
+        ];
+        after = [
+          "devenv-recipes:enterShell:initialize:composer-bin"
+          "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:composer.json"
+          "devenv-recipes:enterShell:install:composer"
+        ];
+        exec = ''
+          set -o 'errexit'
+          cd '${working-dir}'
+          '${composer-binary}' 'bin' '${namespace}' 'install'
+        '';
+      };
+    };
 }
