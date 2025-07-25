8cd0838306bf9bb8ddeb27b29b35c0a3b795218b
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-25 09:26:03 +0200

    feat(nix): ✨ add nil linting task

--- src/nix.nix
+++ src/nix.nix
@@ -10,6 +10,7 @@ in
     nixos-anywhere
     nixos-rebuild
     nixdoc
+    fd
   ];
 
   # https://devenv.sh/languages/
@@ -37,7 +38,7 @@ in
 
   # https://devenv.sh/tasks/
   tasks = {
-    "ci:lint:deadnix" =
+    "ci:lint:nix:deadnix" =
       let
         inherit (config.git-hooks.hooks.deadnix) package;
       in
@@ -45,10 +46,12 @@ in
         description = "Lint *.nix files with deadnix";
         exec = ''
           set -o 'errexit' -o 'pipefail'
-          ${package}/bin/deadnix --output-format 'json' > "$DEVENV_TASK_OUTPUT_FILE"
+
+          cd "''${DEVENV_ROOT}"
+          ${package}/bin/deadnix --output-format 'json' > "''${DEVENV_TASK_OUTPUT_FILE}"
         '';
       };
-    "ci:lint:statix" =
+    "ci:lint:nix:statix" =
       let
         inherit (config.git-hooks.hooks.statix) package;
       in
@@ -56,14 +59,31 @@ in
         description = "Lint *.nix files with statix";
         exec = ''
           set -o 'errexit' -o 'pipefail'
-          ${package}/bin/statix check --format 'json' > "$DEVENV_TASK_OUTPUT_FILE"
+
+          cd "''${DEVENV_ROOT}"
+          ${package}/bin/statix check --format 'json' > "''${DEVENV_TASK_OUTPUT_FILE}"
         '';
       };
-    "ci:format:nixfmt" = {
+    "ci:lint:nix:nil" =
+      let
+        inherit (config.git-hooks.hooks.nil) package;
+      in
+      {
+        description = "Lint *.nix files with nil";
+        exec = ''
+          set -o 'errexit' -o 'pipefail'
+
+          cd "''${DEVENV_ROOT}"
+          ${pkgs.fd}/bin/fd '\.nix$' "''${DEVENV_ROOT}" --exec ${package}/bin/nil diagnostics -- {}
+        '';
+      };
+    "ci:format:nix:nixfmt" = {
       description = "Format *.nix files with nixfmt";
       exec = ''
         set -o 'errexit' -o 'pipefail'
-        ${strict-nixfmt-tree}/bin/treefmt --tree-root '${config.env.DEVENV_ROOT}'
+
+        cd "''${DEVENV_ROOT}"
+        ${strict-nixfmt-tree}/bin/treefmt --tree-root "''${DEVENV_ROOT}"
       '';
     };
   };
