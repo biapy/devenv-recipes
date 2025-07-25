6ffa2fbe9c2590acfb01f16ffa911d53a7c1cfd0
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-25 08:39:04 +0200

    feat(utils): 🏗️ add initializeFiles function to utils.tasks

--- src/utils/tasks.nix
+++ src/utils/tasks.nix
@@ -1,8 +1,64 @@
-{ config, ... }:
-let
-  working-dir = "${config.env.DEVENV_ROOT}";
-in
-{
+_: {
+  /**
+    Create the contents of a bash script that:
+
+    For each `${file}` = `{$sourcePath}` pair in `${configFiles}` attribute set:
+
+    1. Check if `${DEVENV_ROOT}/${file}` exists,
+    2. copy `${sourcePath}` to `${DEVENV_ROOT}/${file}` if not.
+
+    It's helpful for creating example configuration files.
+
+    # Example
+
+    ```nix
+    _: let
+      utils = import ../utils { inherit config; };
+    in {
+      tasks.initializeConfiguration.exec =
+        utils.tasks.initializeFile { "vendor-bin/phpmd/composer.json" = ./phpmd-composer.json; };
+    }
+    ```
+
+    # Type
+
+    ```
+    initializeFile :: Attrset {String = Path;} -> String
+    ```
+
+    # Arguments
+
+    configFiles
+    : a set of file paths and their source paths `{String = Path;}`, where:
+
+      - the key is the initialized file path in the devenv.
+      - the value is the source path in the local nix package.
+  */
+  initializeFiles = configFiles: ''
+    set -o 'errexit'
+
+    function initializeFile() {
+      local file="''${1}"
+      local sourcePath="''${2}"
+
+      file="''${DEVENV_ROOT}/''${file}"
+      filepath="$(dirname "''${file}")"
+
+      [[ -e "''${file}" ]] && return 0
+
+      mkdir --parent "''${filepath}" &&
+      cp "''${sourcePath}" "''${file}"
+    }
+
+    ${
+      (builtins.concatStringsSep "\n" (
+        builtins.attrValues (
+          builtins.mapAttrs (file: sourcePath: ''initializeFile "${file}" "${sourcePath}"'') configFiles
+        )
+      ))
+    }
+  '';
+
   /**
     Create the contents of a bash script that:
 
@@ -39,13 +95,13 @@ in
   initializeFile = file: sourcePath: ''
     set -o 'errexit'
 
-    file='${working-dir}/${file}'
-    filepath="$(dirname "$file")"
+    file="''${DEVENV_ROOT}/${file}"
+    filepath="$(dirname "''${file}")"
 
-    [[ -e "$file" ]] && exit 0
+    [[ -e "''${file}" ]] && exit 0
 
-    mkdir --parent "$filepath" &&
-    cp -a "${sourcePath}" "$file"
+    mkdir --parent "''${filepath}" &&
+      cp "${sourcePath}" "''${file}"
   '';
 
   /**
@@ -90,13 +146,13 @@ in
   initializeFileContents = file: contents: ''
     set -o 'errexit'
 
-    file='${working-dir}/${file}'
-    filepath="$(dirname "$file")"
+    file="''${DEVENV_ROOT}/${file}"
+    filepath="$(dirname "''${file}")"
 
-    [[ -e "$file" ]] && exit 0
+    [[ -e "''${file}" ]] && exit 0
 
-    mkdir --parent "$filepath" &&
-    tee "$file" << EOF
+    mkdir --parent "''${filepath}" &&
+    tee "''${file}" << EOF
     ${contents}
     EOF
   '';
