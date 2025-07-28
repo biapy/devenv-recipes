294da9b0c410154af948940718e32bcafcf5033f
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-28 12:14:33 +0200

    fix(php): 🐛 add /vendor-bin/**/vendor/ to .gitignore in composer-bin recipe

--- src/php/composer-bin.nix
+++ src/php/composer-bin.nix
@@ -2,32 +2,39 @@
 # @see https://github.com/bamarni/composer-bin-plugin
 { config, ... }:
 let
+  utils = import ../utils { inherit config; };
   composerCommand = "${config.languages.php.packages.composer}/bin/composer";
 in
 {
   imports = [ ./composer.nix ];
 
   # https://devenv.sh/tasks/
-  tasks = {
-    "devenv-recipes:enterShell:initialize:composer-bin" = {
-      description = "Require 'bamarni/composer-bin-plugin' if missing";
-      before = [
-        "devenv:enterShell"
-        "devenv-recipes:enterShell:install:composer"
-      ];
-      exec = ''
-        set -o 'errexit' -o 'pipefail'
+  tasks =
+    {
+      "devenv-recipes:enterShell:initialize:composer-bin" = {
+        description = "Require 'bamarni/composer-bin-plugin' if missing";
+        before = [
+          "devenv:enterShell"
+          "devenv-recipes:enterShell:install:composer"
+        ];
+        exec = ''
+          set -o 'errexit' -o 'pipefail'
 
-        [[ -e "''${DEVENV_ROOT}/composer.json" ]] || exit 0
-        if grep --quiet 'bamarni/composer-bin-plugin' "''${DEVENV_ROOT}/composer.json"; then
-          exit 0
-        fi
+          [[ -e "''${DEVENV_ROOT}/composer.json" ]] || exit 0
+          if grep --quiet 'bamarni/composer-bin-plugin' "''${DEVENV_ROOT}/composer.json"; then
+            exit 0
+          fi
 
-        cd "''${DEVENV_ROOT}"
-        ${composerCommand} config --json 'allow-plugins.bamarni/composer-bin-plugin' 'true'
-        ${composerCommand} config --json 'extra.bamarni-bin.bin-links' 'false'
-        ${composerCommand} require --dev 'bamarni/composer-bin-plugin'
-      '';
+          cd "''${DEVENV_ROOT}"
+          ${composerCommand} config --json 'allow-plugins.bamarni/composer-bin-plugin' 'true'
+          ${composerCommand} config --json 'extra.bamarni-bin.bin-links' 'false'
+          ${composerCommand} require --dev 'bamarni/composer-bin-plugin'
+        '';
+      };
+    }
+    // utils.tasks.gitIgnoreTask {
+      name = "Composer bin plugin";
+      namespace = "composer-bin";
+      ignoredPaths = [ "/vendor-bin/**/vendor/" ];
     };
-  };
 }
