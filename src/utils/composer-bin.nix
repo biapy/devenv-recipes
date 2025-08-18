e93b0e9a7ada27f2b3b23051683938b331e66cd9
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-08-18 22:20:14 +0200

    fix(utils): :bug: add status checks to composer-bin.nix tasks

--- src/utils/composer-bin.nix
+++ src/utils/composer-bin.nix
@@ -1,7 +1,18 @@
-{ config, ... }:
+/**
+  # Functions for composer bin tools
+
+  ## 🙇 Acknowledgements
+
+  - [lib.meta.getExe @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.getExe).
+  - [builtins.attrNames @ Nix 2.28.5 Reference Manual](https://nix.dev/manual/nix/2.28/language/builtins.html#builtins-attrNames).
+  - [builtins.map @ Nix 2.28.5 Reference Manual](https://nix.dev/manual/nix/2.28/language/builtins.html#builtins-map).
+  - [lib.strings.concatStringsSep @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.strings.concatStringsSep).
+*/
+{ config, lib, ... }:
 let
   tasks = import ./tasks.nix { inherit config; };
-  composerCommand = "${config.languages.php.packages.composer}/bin/composer";
+  inherit (config.languages.php.packages) composer;
+  composerCommand = lib.meta.getExe composer;
 in
 {
 
@@ -16,6 +27,7 @@ in
       "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:composer-json" = {
         description = "Initialize ${name} composer.json";
         before = [ "devenv:enterShell" ];
+        status = ''test -e "''${DEVENV_ROOT}/vendor-bin/${namespace}/composer.json"'';
         exec = tasks.initializeFile "vendor-bin/${namespace}/composer.json" composerJsonPath;
       };
     };
@@ -31,6 +43,11 @@ in
       "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:configuration" = {
         description = ''Initialize ${name} configuration file(s)'';
         before = [ "devenv:enterShell" ];
+        status = ''test ${
+          lib.strings.concatStringsSep " -a " (
+            builtins.map (file: ''-e "''${DEVENV_ROOT}/${file}"'') (builtins.attrNames configFiles)
+          )
+        }'';
         exec = tasks.initializeFiles configFiles;
       };
     };
@@ -49,8 +66,8 @@ in
           "devenv-recipes:enterShell:initialize:composer-bin:${namespace}:composer-json"
           "devenv-recipes:enterShell:install:composer"
         ];
-        # Use the composer command to install the too
-
+        status = ''test -e "''${DEVENV_ROOT}/vendor-bin/${namespace}/vendor/autoload.php"'';
+        # Use the composer command to install the tool
         exec = ''
           set -o 'errexit'
           cd "''${DEVENV_ROOT}"
