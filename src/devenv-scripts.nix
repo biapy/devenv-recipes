febcddf40a7c8965b05910ab6dd0598709b938b4
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-08-20 09:19:14 +0200

    refactor: :adhesive_bandage: add brackets around vars in scripts

--- src/devenv-scripts.nix
+++ src/devenv-scripts.nix
@@ -1,11 +1,35 @@
+/**
+  # devenv scripts
+
+  Scripts to ease devenv use.
+
+  ## 🛠️ Tech Stack
+
+  - [devenv homepage](https://devenv.sh/).
+  - [direnv homepage](https://direnv.net/).
+
+  ### Visual Studio Code
+
+  - [direnv @ Visual Studio Code Marketplace](https://marketplace.visualstudio.com/items?itemName=mkhl.direnv).
+
+  ### Third party tools
+
+  - [fd @ GitHub](https://github.com/sharkdp/fd).
+
+  ## 🙇 Acknowledgements
+
+  - [devcontainer @ Devenv Reference Manual](https://devenv.sh/reference/options/#devcontainerenable).
+*/
 _: {
+  devcontainer.settings.customizations.vscode.extensions = [ "mkhl.direnv" ];
+
   scripts = {
     detr = {
       description = "Alias of devenv tasks run";
       exec = ''
         set -o 'errexit' -o 'nounset' -o 'pipefail'
-        cd "$DEVENV_ROOT"
-        devenv tasks run "$@"
+        cd "''${DEVENV_ROOT}"
+        devenv tasks run "''${@}"
       '';
     };
   };
