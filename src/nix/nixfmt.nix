e07faa4f2d900d389745df89927dcf8c94324030
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-08-20 07:51:05 +0200

    style(nix): :art: apply nixfmt strict formating to nixfmt module

--- src/nix/nixfmt.nix
+++ src/nix/nixfmt.nix
@@ -1,25 +1,21 @@
 /**
-    # Nixfmt
+  # Nixfmt
 
   Nixfmt is the official formatter for Nix language code.
 
-    ## 🛠️ Tech Stack
+  ## 🛠️ Tech Stack
 
-    - [Nixfmt @ GitHub](https://github.com/NixOS/nixfmt).
-    - [treefmt homepage](https://treefmt.com/latest/)
-      ([treefmt @ GitHub](https://github.com/numtide/treefmt)).
-    - [nixfmt-tree @ NixPkgs' GitHub](https://github.com/NixOS/nixpkgs/tree/nixos-25.05/pkgs/by-name/ni/nixfmt-tree).
+  - [Nixfmt @ GitHub](https://github.com/NixOS/nixfmt).
+  - [treefmt homepage](https://treefmt.com/latest/)
+    ([treefmt @ GitHub](https://github.com/numtide/treefmt)).
+  - [nixfmt-tree @ NixPkgs' GitHub](https://github.com/NixOS/nixpkgs/tree/nixos-25.05/pkgs/by-name/ni/nixfmt-tree).
 
-    ## 🙇 Acknowledgements
+  ## 🙇 Acknowledgements
 
-    - [lib.meta.getExe @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.getExe).
-    - [git-hooks.hooks.nixfmt-rfc-style @ Devenv Reference Manual](https://devenv.sh/reference/options/#git-hookshooksnixfmt-rfc-style).
+  - [lib.meta.getExe @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.getExe).
+  - [git-hooks.hooks.nixfmt-rfc-style @ Devenv Reference Manual](https://devenv.sh/reference/options/#git-hookshooksnixfmt-rfc-style).
 */
-{
-  pkgs,
-  lib,
-  ...
-}:
+{ pkgs, lib, ... }:
 let
   strict-nixfmt-tree = pkgs.nixfmt-tree.override { settings.formatter.nixfmt.options = "--strict"; };
   nixfmtTreeCommand = lib.meta.getExe strict-nixfmt-tree;
@@ -28,13 +24,9 @@ in
   imports = [ ./nix.nix ];
 
   # https://devenv.sh/packages/
-  packages = [
-    strict-nixfmt-tree
-  ];
+  packages = [ strict-nixfmt-tree ];
 
-  devcontainer.settings.customizations.vscode.extensions = [
-    "brettm12345.nixfmt-vscode"
-  ];
+  devcontainer.settings.customizations.vscode.extensions = [ "brettm12345.nixfmt-vscode" ];
 
   # https://devenv.sh/git-hooks/
   git-hooks.hooks.nixfmt-rfc-style = {
@@ -51,5 +43,4 @@ in
       ${nixfmtTreeCommand} --tree-root "''${DEVENV_ROOT}"
     '';
   };
-
 }
