c036c48bc946a5ba2d86c4c2f496fcd452ca7503
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-08-24 10:15:56 +0200

    feat(php): :sparkles: add XDebug extension, and php-get-ini script

--- src/php/php.nix
+++ src/php/php.nix
@@ -8,6 +8,7 @@
 
   ### 🐚 Commands
 
+  - `php-get-ini`: Get PHP ini configuration.
   - `php-modules`: List installed PHP modules.
 
   ## 🛠️ Tech Stack
@@ -17,6 +18,8 @@
   ## 🙇 Acknowledgements
 
   - [languages.php @ devenv](https://devenv.sh/reference/options/#languagesphpenable).
+  - [scripts @ Devenv Reference Manual](https://devenv.sh/reference/options/#scripts).
+  - [lib.meta.getExe @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.getExe).
   - [lib.strings.concatStringsSep @ Nixpkgs Reference Manual](https://nixos.org/manual/nixpkgs/stable/#function-library-lib.strings.concatStringsSep).
 */
 { config, lib, ... }:
@@ -29,30 +32,65 @@ in
   languages.php = {
     enable = true;
 
+    extensions = [ "xdebug" ];
+
     ini = lib.concatStringsSep "\n" [
       "xdebug.mode = develop"
       "memory_limit = 256m"
     ];
-
   };
+
   # https://devenv.sh/scripts/
-  scripts.php-modules.exec = ''
-    set -o 'errexit' -o 'pipefail'
+  scripts = {
+    php-modules = {
+      description = "List installed PHP modules";
+      exec = ''
+        set -o 'errexit' -o 'pipefail'
 
-    echo 'Installed PHP modules'
-    echo '---------------------'
+        echo 'Installed PHP modules'
+        echo '---------------------'
 
-    ${phpCommand} -m |
-    command grep --invert-match --extended-regexp '^(|\[.*\])$' |
-    command tr '\n' ' ' |
-    command fold --spaces
+        ${phpCommand} -m |
+        command grep --invert-match --extended-regexp '^(|\[.*\])$' |
+        command tr '\n' ' ' |
+        command fold --spaces
 
-    printf '\n---------------------\n'
-  '';
+        printf '\n---------------------\n'
+      '';
+    };
+    php-get-ini = {
+      description = "Get PHP ini configuration";
+      exec = ''
+        set -o 'errexit' -o 'pipefail'
+
+        print-ini-value() {
+          local name="''${1}"
+
+          php --run "printf(\"''${name} = %s\n\",(string) ini_get(\"''${name}\"));"
+        }
+
+        php-get-ini() {
+          # Check that arg are given in bash
+          if [ "''${#}" -eq 0 ]; then
+            echo "Usage: php-get-ini <name> [name ...]"
+            return 1
+          fi
+          local names="''${@}"
+
+          for name in ''${names}; do
+            print-ini-value "''${name}"
+          done
+        }
+
+        php-get-ini "''${@}"
+      '';
+    };
+  };
 
   enterShell = ''
     php --version
     php-modules
+    php-get-ini 'memory_limit' 'xdebug.mode' 'xdebug.client_host' 'xdebug.client_port'
   '';
 
   # See full reference at https://devenv.sh/reference/options/
