a8b307e208067720954a7232d733a9e48360d706
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-08-26 21:12:05 +0200

    docs(secrets): :memo: update bws docblock

--- src/secrets/bws.nix
+++ src/secrets/bws.nix
@@ -6,9 +6,15 @@
   The Secrets Manager CLI can be used to organize your vault with create,
   delete, edit, and list your secrets and projects.
 
+  Set the API key in `BWS_ACCESS_TOKEN` (in the project `.env.local` with dotenv).
+
+  ❗ Requires `allowUnfree: true` in `devenv.yaml`.
+
+  ⚠️ Vaultwarden doesn't support `bws`. It's a commercial Bitwarden feature.
+
   ## 🛠️ Tech Stack
 
-  - [Secrets Manager CLI @ BitWarden]()
+  - [Secrets Manager CLI @ BitWarden](https://bitwarden.com/help/secrets-manager-cli/)
     ([Bitwarden Secrets Manager SDK @ GitHub](https://github.com/bitwarden/sdk-sm)).
 */
 { pkgs, ... }:
