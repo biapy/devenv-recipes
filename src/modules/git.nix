a3c4725fe212f1524fcf237ff3f645acc234825d
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-10-22 07:53:06 +0200

    refactor(git): ♻ refactor git recipe into a Nix module

---
+++ src/modules/git.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption mkPackageOption;
+  inherit (pkgs) lazygit;
+
+  cfg = config.biapy-recipes.git;
+
+  git = cfg.package;
+in
+{
+
+  options.biapy-recipes.git = {
+    enable = mkEnableOption "Git";
+    package = mkPackageOption pkgs "Git" { default = "git"; };
+  };
+
+  config = mkIf cfg.enable {
+    # https://devenv.sh/packages/
+    packages = [
+      git
+      lazygit # Git terminal UI
+    ];
+
+    # https://devenv.sh/integrations/difftastic/
+    difftastic.enable = true;
+
+    # https://devenv.sh/integrations/delta/
+    delta.enable = true;
+
+    enterShell = ''
+      git --version
+      lazygit --version
+    '';
+
+    # https://devenv.sh/tasks/
+    tasks = {
+      "devenv-recipes:enterTest:git-version" = {
+        description = "Test available git command version match devenv git package";
+        before = [ "devenv:enterTest" ];
+        exec = ''
+          set -o 'errexit' -o 'pipefail'
+          git --version | grep --color=auto "${git.version}"
+        '';
+      };
+    };
+  };
+}
